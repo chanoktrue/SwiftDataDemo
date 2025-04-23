9133356dd92265d02a5e43ff8daea659a963b0d9
Thongchai Subsaidee <chanoktrue@me.com>
2025-04-24 05:50:51 +0700

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Thongchai Subsaidee on 24/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
